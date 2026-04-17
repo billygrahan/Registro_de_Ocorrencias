378c1cac13dda15dfd517645b6c16645b35d4350
billygrahan <billygrahan09@gmail.com>
2026-04-17 17:37:21 -0300

    fix: Remove invalid CORS config from GraphQL and use Express CORS middleware

--- Back_End/src/app.module.ts
+++ Back_End/src/app.module.ts
@@ -7,6 +7,7 @@ import { MachineModule } from './modules/machine/machine.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { AppController } from './app.controller'
 import { join } from 'path'
+import { Request } from 'express'
 
 @Module({
     imports: [
@@ -17,7 +18,10 @@ import { join } from 'path'
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-            sortSchema: true
+            sortSchema: true,
+            context: ({ req }: { req: Request }) => {
+                return { req };
+            },
         }),
         AuthModule,
         IncidenteModule,
