17085aeb5ee330900d90faf68823665c4e033c39
billygrahan <billygrahan09@gmail.com>
2026-04-18 18:19:59 -0300

    playground: false,             plugins: isLandingPageEnabled                 ? [ApolloServerPluginLandingPageLocalDefault({ embed: true })]                 : [],

--- Back_End/src/app.module.ts
+++ Back_End/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { IncidenteModule } from './modules/incidente/incidente.module'
 import { MachineModule } from './modules/machine/machine.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { AppController } from './app.controller'
 import { join } from 'path'
 import { Request } from 'express'
+
+const isLandingPageEnabled = process.env.GRAPHQL_LANDING_PAGE_ENABLED !== 'false'
 
 @Module({
     imports: [
@@ -21,7 +24,10 @@ import { Request } from 'express'
             sortSchema: true,
             csrfPrevention: false,
             introspection: true,
-            playground: process.env.NODE_ENV !== 'production',
+            playground: false,
+            plugins: isLandingPageEnabled
+                ? [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+                : [],
             context: ({ req }: { req: Request }) => {
                 return { req };
             },
